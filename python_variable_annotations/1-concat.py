b491e0091c368338b231e4813bd8c315e66b4a5d
Claire2311 <claire.girard@ieseg.fr>
2024-07-29 10:56:50 +0200

    Task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+function that takes two arguments and return a concatened string
+>>> concat("egg", "shell")
+egg shell
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """return a concatened string of a and b which are string"""
+    return str1 + ' ' + str2
