bf4db9eed185705bf175629c6fd81be7d2e281d3
Claire2311 <claire.girard@ieseg.fr>
2024-07-29 11:47:12 +0200

    Task 6

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+function that a list of floats and integer as argument and returns their sum
+>>> sum_mixed_list([5, 4, 3.14, 666, 0.99])
+679.13
+"""
+
+from typing import List
+
+
+def sum_mixed_list(input_list: List[float | int]) -> float:
+    """return the sum of floats and integer"""
+    a = 0
+    for number in input_list:
+        a += number
+    return a
