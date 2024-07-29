2634e63f3267bcf768c443c55d24f3a3a1db27a9
Claire2311 <claire.girard@ieseg.fr>
2024-07-29 11:40:55 +0200

    Task 5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+function that a list of floats as argument and returns their sum
+>>> sum_list([3.14, 1.11, 2.22])
+6.470000000000001
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """return the sum of floats"""
+    a = 0
+    for number in input_list:
+        a += number
+    return a
