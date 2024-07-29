f0cc1b0e63959d6a3db43e1886e99a8f129eac84
Claire2311 <claire.girard@ieseg.fr>
2024-07-29 11:12:43 +0200

    Task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+function that takes one float argument and return the string representation of
+the float
+>>> to_str(3.14)
+'3.14'
+"""
+
+
+def to_str(n: float) -> str:
+    """return the string representation of the float n"""
+    return n
