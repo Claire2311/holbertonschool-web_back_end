89dcadbae6c4065fdc5bb6696c8aba54cd8a07ff
Claire2311 <claire.girard@ieseg.fr>
2024-07-29 15:37:48 +0200

    Task 11

--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -5,9 +5,11 @@ function that returns element of a dictionnary
 
 from typing import Mapping, Any, Union, TypeVar
 
+T = TypeVar('T')
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[TypeVar, None]
-                     = None):
+
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None]
+                     = None) -> Union[Any, T]:
     """function that returns element of a dictionnary"""
     if key in dct:
         return dct[key]
