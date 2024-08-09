3dc7aa03ce3baa422fcafc05ddfe0e1edee124d7
Claire2311 <claire.girard@ieseg.fr>
2024-08-09 14:06:50 +0200

    Task 1

---
+++ caching/1-fifo_cache.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+""" FIFO caching
+"""
+
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """extends the BaseCaching class"""
+    def __init__(self):
+        super().__init__()
+        self.cache_data = {}
+
+    def put(self, key, item):
+        """add key/value in the dictionnary"""
+        if not key or not item:
+            return
+        self.cache_data[key] = item
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            print("DISCARD: {}".format(next(iter(self.cache_data))))
+            self.cache_data.pop(next(iter(self.cache_data)))
+            # print(next(iter(self.cache_data)))
+            # for key in self.cache_data:
+            #     print(key)
+
+    def get(self, key):
+        """get value according to the given key"""
+        if key not in self.cache_data or key is None:
+            return None
+        return self.cache_data[key]
