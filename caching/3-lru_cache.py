61149ebdcf951ae8dc45df26ac2a318a387e0988
Claire2311 <claire.girard@ieseg.fr>
2024-08-09 14:44:59 +0200

    Task 3

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" FIFO caching
+""" LRU caching
 """
 
 
@@ -37,31 +37,3 @@ class LRUCache(BaseCaching):
         self.cache_data[key] = temporary_variable
 
         return self.cache_data[key]
-
-
-my_cache = LRUCache()
-my_cache.put("A", "Hello")
-my_cache.put("B", "World")
-my_cache.put("C", "Holberton")
-my_cache.put("D", "School")
-my_cache.print_cache()
-print(my_cache.get("B"))
-my_cache.put("E", "Battery")
-my_cache.print_cache()
-my_cache.put("C", "Street")
-my_cache.print_cache()
-print(my_cache.get("A"))
-print(my_cache.get("B"))
-print(my_cache.get("C"))
-my_cache.put("F", "Mission")
-my_cache.print_cache()
-my_cache.put("G", "San Francisco")
-my_cache.print_cache()
-my_cache.put("H", "H")
-my_cache.print_cache()
-my_cache.put("I", "I")
-my_cache.print_cache()
-my_cache.put("J", "J")
-my_cache.print_cache()
-my_cache.put("K", "K")
-my_cache.print_cache()
