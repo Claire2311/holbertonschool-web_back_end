3ab9311ee22f089dee74f3e8d555f9324e768c6c
Claire2311 <claire.girard@ieseg.fr>
2024-08-09 14:30:25 +0200

    Task 2

---
+++ caching/2-lifo_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+""" FIFO caching
+"""
+
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """extends the BaseCaching class"""
+    def __init__(self):
+        super().__init__()
+        self.cache_data = {}
+
+    def put(self, key, item):
+        """add key/value in the dictionnary"""
+        if not key or not item:
+            return
+        if key in self.cache_data:
+            self.cache_data.pop(key)
+            self.cache_data[key] = item
+
+        if key not in self.cache_data and \
+                len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            print("DISCARD: {}".format(list(self.cache_data)[-1]))
+            self.cache_data.popitem()
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """get value according to the given key"""
+        if key not in self.cache_data or key is None:
+            return None
+        return self.cache_data[key]
