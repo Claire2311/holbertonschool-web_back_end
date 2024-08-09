507c7b900330da84fb482f18c46c64d91f6dcda5
Claire2311 <claire.girard@ieseg.fr>
2024-08-09 13:32:22 +0200

    Task 0

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" Create a basic dictionnary
+"""
+
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """extends the BaseCaching class"""
+    def __init__(self):
+        super().__init__()
+        self.cache_data = {}
+
+    def put(self, key, item):
+        """add key/value in the dictionnary"""
+        if not key or not item:
+            pass
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """get value according to the given key"""
+        if key in self.cache_data or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
---
+++ caching/README.md
@@ -0,0 +1,14 @@
+# Caching
+
+It is a project to learn different caching algorithms.
+
+The objectives are:
+
+    What a caching system is
+    What FIFO means
+    What LIFO means
+    What LRU means
+    What MRU means
+    What LFU means
+    What the purpose of a caching system
+    What limits a caching system have
