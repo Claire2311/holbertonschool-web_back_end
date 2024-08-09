0fdd21450a88143a6e955d9c466e495255026fa8
Claire2311 <claire.girard@ieseg.fr>
2024-08-09 13:39:32 +0200

    Task 0

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -20,6 +20,24 @@ class BasicCache(BaseCaching):
 
     def get(self, key):
         """get value according to the given key"""
-        if key in self.cache_data or key not in self.cache_data:
+        if key not in self.cache_data or key not in self.cache_data:
             return None
         return self.cache_data[key]
+
+
+my_cache = BasicCache()
+my_cache.print_cache()
+my_cache.put("A", "Hello")
+my_cache.put("B", "World")
+my_cache.put("C", "Holberton")
+my_cache.print_cache()
+print(my_cache.get("A"))
+print(my_cache.get("B"))
+print(my_cache.get("C"))
+print(my_cache.get("D"))
+my_cache.print_cache()
+my_cache.put("D", "School")
+my_cache.put("E", "Battery")
+my_cache.put("A", "Street")
+my_cache.print_cache()
+print(my_cache.get("A"))
