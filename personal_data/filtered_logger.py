eb6bc3490b15ff443186fec2f6b48ff9b79d7993
Claire2311 <claire.girard@ieseg.fr>
2024-09-20 13:08:43 +0200

    Task 0

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 """ return the log message obfuscated"""
 
+from typing import List
 import re
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(
+    fields: List[str], redaction: str, message: str, separator: str
+) -> str:
     """return the log message obfuscated"""
     log_msges = message.split(separator)
     for index, log_msg in enumerate(log_msges):
