4403d4a42ab1c422041bb96fa5ddca6fbf4cc5bc
Claire2311 <claire.girard@ieseg.fr>
2024-09-23 11:20:01 +0200

    Task 5

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -7,7 +7,7 @@ import bcrypt
 
 def hash_password(password: str) -> bytes:
     """hash a password"""
-    return bcrypt.hashpw(password, bcrypt.gensalt())
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
