cbc7f286fc7380ef6fd433a270d206aa33d46537
Claire2311 <claire.girard@ieseg.fr>
2024-09-23 16:03:56 +0200

    Task 1

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,9 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/api/v1/unauthorized', methods=['GET'], strict_slashes=False)
+def get_unauthorized_error():
+    """GET an error message when unauthorized"""
+    abort(401)
