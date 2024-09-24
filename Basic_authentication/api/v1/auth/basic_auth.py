591458c9d149373e4350df0d1233832b9efea001
Claire2311 <claire.girard@ieseg.fr>
2024-09-24 12:38:20 +0200

    Task 8

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """class to handle basic authentication"""
 
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -20,3 +21,21 @@ class BasicAuth(Auth):
             return None
         else:
             return authorization_header.split("Basic ")[1]
+
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
+        """
+        returns the decoded value of a Base64 string 
+        base64_authorization_header
+        """
+        if (base64_authorization_header is None or 
+                not isinstance(base64_authorization_header, str)):
+            return None
+
+        try:
+            base64.b64encode(base64.b64decode(base64_authorization_header)) == base64_authorization_header
+        except Exception:
+            return None
+        else:
+            return base64_authorization_header.decode('utf-8')
