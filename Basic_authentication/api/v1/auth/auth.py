c1380945bb27bff15ec88f41a5540e3aa57f195c
Claire2311 <claire.girard@ieseg.fr>
2024-09-24 11:51:09 +0200

    Task 5

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -14,7 +14,7 @@ class Auth:
         if path is None:
             return True
 
-        if excluded_paths is None or not excluded_paths:
+        if excluded_paths is None or len(excluded_paths) == 0:
             return True
 
         new_path: str
@@ -26,6 +26,8 @@ class Auth:
 
         if new_path in excluded_paths:
             return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """manager authorization header"""
