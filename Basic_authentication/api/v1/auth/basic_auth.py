2404c6adfce9c759de57a87b03eab740bdddfd25
Claire2311 <claire.girard@ieseg.fr>
2024-09-24 12:54:35 +0200

    Task 9

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -45,7 +45,7 @@ class BasicAuth(Auth):
         """returns the user email and password from the Base64 decoded value"""
         if (decoded_base64_authorization_header is None or
                 not isinstance(decoded_base64_authorization_header, str) or
-                '-' not in decoded_base64_authorization_header):
+                ':' not in decoded_base64_authorization_header):
             return None, None
         else:
             user_credentials = decoded_base64_authorization_header.split(":")
