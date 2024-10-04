70d341594965fb5a92cfff6868b06c9926608063
Claire2311 <claire.girard@ieseg.fr>
2024-10-04 15:16:08 +0200

    Task 7

---
+++ Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+""" Module of session views
+"""
+from os import getenv
+from api.v1.views import app_views
+from flask import abort, jsonify, request
+from models.user import User
+
+
+@app_views.route('/auth_session/login', methods=['POSR'], strict_slashes=False)
+def view_auth() -> str:
+    """ POST /api/v1/auth_session/login
+    Return:
+      -
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not email or email == '':
+        return jsonify({"error": "email missing"}), 400
+
+    if not password or password == '':
+        return jsonify({"error": "password missing"}), 400
+
+    user = User()
+    try:
+        user_search = User.search({'email': email})
+    except Exception:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    for user in user_search:
+        if not user.is_valid_password(password):
+            return jsonify({"error": "wrong password"}), 404
+
+    # if not user:
+    #     return jsonify({"error": "no user found for this email"}), 404
+
+    # if not User.is_valid_password(password):
+    #     return jsonify({"error": "wrong password"}), 404
+
+    if user_search:
+        from api.v1.app import auth
+        session_id = auth.create_session(user_search[0].id)
+        cookie_name = getenv('SESSION_NAME')
+        response_to_send = user.to_json()
+        response_to_send.set_cookie(cookie_name, session_id)
+        return response_to_send
