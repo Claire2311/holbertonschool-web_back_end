11f5640c5f4ab32133bd25c0b4cbc46ef634cbff
Claire2311 <claire.girard@ieseg.fr>
2024-10-04 15:32:08 +0200

    Task 7

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -7,7 +7,7 @@ from flask import abort, jsonify, request
 from models.user import User
 
 
-@app_views.route('/auth_session/login', methods=['POSR'], strict_slashes=False)
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def view_auth() -> str:
     """ POST /api/v1/auth_session/login
     Return:
@@ -23,9 +23,10 @@ def view_auth() -> str:
         return jsonify({"error": "password missing"}), 400
 
     user = User()
-    try:
-        user_search = User.search({'email': email})
-    except Exception:
+    # try:
+    user_search = User.search({'email': email})
+    # except Exception:
+    if not user_search:
         return jsonify({"error": "no user found for this email"}), 404
 
     for user in user_search:
@@ -42,6 +43,8 @@ def view_auth() -> str:
         from api.v1.app import auth
         session_id = auth.create_session(user_search[0].id)
         cookie_name = getenv('SESSION_NAME')
-        response_to_send = user.to_json()
+        response_to_send = jsonify(user.to_json())
+        print('response_to_send', response_to_send)
+        print(type(response_to_send))
         response_to_send.set_cookie(cookie_name, session_id)
         return response_to_send
