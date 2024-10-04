46aa450801592345a11141ad88439b38fe278fef
Claire2311 <claire.girard@ieseg.fr>
2024-10-04 12:29:42 +0200

    Task 1

---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""class to handle basic authentication"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """new authentication mechanism"""
+    pass
