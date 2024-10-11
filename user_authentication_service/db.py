a87347d4105958fd2e1f537648f1fbf5764d3eb4
Claire2311 <claire.girard@ieseg.fr>
2024-10-11 17:17:53 +0200

    fil db add typing

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """DB module
 """
+from typing import Any
 from sqlalchemy import create_engine, insert, Column, inspect
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -32,14 +33,14 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password) -> User:
+    def add_user(self, email: str, hashed_password: str) -> User:
         """add an user in the DB"""
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
         return user
 
-    def find_user_by(self, **kwargs) -> User:
+    def find_user_by(self, **kwargs: dict[str, Any]) -> User:
         """
         takes in arbitrary keyword arguments and returns the first row found
         in the users table as filtered by the method’s input arguments
@@ -54,7 +55,7 @@ class DB:
         except InvalidRequestError as e:
             raise InvalidRequestError("Invalid query arguments") from e
 
-    def update_user(self, user_id: int, **kwargs) -> None:
+    def update_user(self, user_id: int, **kwargs: dict[str, Any]) -> None:
         """Update a user in the DB"""
         try:
             user = self.find_user_by(id=user_id)
