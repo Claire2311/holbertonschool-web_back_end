56c656e03350743162c8201ec3ab18e93b8eeda8
Claire2311 <claire.girard@ieseg.fr>
2024-10-11 18:21:37 +0200

    Task 7

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,14 +2,29 @@
 """
 Route module for the API
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+
+AUTH = Auth()
 
 
 @app.route('/')
 def welcome():
     return jsonify({"message": "Bienvenue"})
+
+@app.route('/users', methods=['POST'])
+def users():
+    """register user"""
+    email = request.form['email']
+    password = request.form['password']
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    if user:
+        return jsonify({"email": email, "message": "user created"})
 
 
 if __name__ == "__main__":
