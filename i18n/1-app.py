3c2ca28f8da437dcb7c14e0726159ebcf7bf115f
Claire2311 <claire.girard@ieseg.fr>
2024-11-15 10:51:26 +0100

    Task 1

--- i18n/1-app.py
+++ i18n/1-app.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 """ App module
 """
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
+
+def get_locale():
+    """get the default language"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 app = Flask(__name__)
-babel = Babel(app, locale_selector="en")
 
 
 @app.route("/")
@@ -19,6 +24,8 @@ class Config:
     """configuration of languages"""
     LANGUAGES = ["en", "fr"]
 
+
+babel = Babel(app, locale_selector="en",  timezone_selector="UTC")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="8000")
