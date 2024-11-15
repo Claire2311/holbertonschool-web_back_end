aca2e83b3ce19ac13440a794a12a5c8655527821
Claire2311 <claire.girard@ieseg.fr>
2024-11-15 13:28:27 +0100

    Task 4

---
+++ i18n/4-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+""" App module
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """configuration of languages"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+def get_locale():
+    """determine the best match with our supported languages"""
+    locale = request.args.get('locale')
+    if locale is not None and locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+babel.init_app(app, locale_selector=get_locale)
+
+
+@app.route("/")
+def hello() -> str:
+    """ return index template
+    """
+    return render_template("4-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="8000")
