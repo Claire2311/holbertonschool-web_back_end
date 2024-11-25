6466b4b000592d40878312384dd27fd323f3a03b
Claire2311 <claire.girard@ieseg.fr>
2024-11-25 13:50:55 +0100

    Task 2

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -3,7 +3,16 @@
 import uuid
 from typing import Union, Optional
 from collections.abc import Callable
+from functools import wraps
 import redis
+
+
+def count_calls(fn: Callable) -> Callable:
+    """count how many times methods of the Cache class are called"""
+    @wraps(fn)
+    def wrapper(self):
+        key = Cache.store.__qualname__
+        self._redis.incr(key)
 
 
 class Cache:
@@ -13,11 +22,11 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """generate a key, store the input data in Redis and return the key"""
         key = str(uuid.uuid4())
-        cache = Cache()
-        cache._redis.set(key, data)
+        self._redis.set(key, data)
         return key
 
     def get(self, key: Union[str, int], fn: Optional[Callable] = None):
