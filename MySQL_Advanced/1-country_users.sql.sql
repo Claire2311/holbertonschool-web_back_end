9d71b8df19e8a10b7b4b0680e774a0cdc1738167
Claire2311 <claire.girard@ieseg.fr>
2024-11-25 17:00:13 +0100

    Task 1

---
+++ MySQL_Advanced/1-country_users.sql.sql
@@ -0,0 +1,12 @@
+-- Active: 1689179452290@@127.0.0.1@3306@holberton
+-- Create a table
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    `name` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    `country` ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+    PRIMARY KEY (`id`),
+    UNIQUE (`email`)
+) ENGINE = InnoDB AUTO_INCREMENT = 16 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
