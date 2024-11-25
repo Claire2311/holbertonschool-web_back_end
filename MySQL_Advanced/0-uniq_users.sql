69356a9f17f15559dfca56197211834eded31c6f
Claire2311 <claire.girard@ieseg.fr>
2024-11-25 16:56:56 +0100

    Task 0 add capital letters

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,10 +1,11 @@
+-- Active: 1689179452290@@127.0.0.1@3306@holberton
 -- Create a table
 DROP TABLE IF EXISTS `users`;
 
 CREATE TABLE `users` (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-    `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    `name` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
     PRIMARY KEY (`id`),
     UNIQUE (`email`)
 ) ENGINE = InnoDB AUTO_INCREMENT = 16 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
