3a9691248869c2c7882de81d32745f4d561c5421
Claire2311 <claire.girard@ieseg.fr>
2024-11-26 15:35:44 +0100

    Task 0

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -4,8 +4,8 @@ DROP TABLE IF EXISTS `users`;
 
 CREATE TABLE `users` (
     `id` INT NOT NULL AUTO_INCREMENT,
-    `email` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-    `name` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `name` VARCHAR(255),
     PRIMARY KEY (`id`),
     UNIQUE (`email`)
-) ENGINE = InnoDB AUTO_INCREMENT = 16 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
