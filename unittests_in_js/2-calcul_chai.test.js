215e5884860f3cde6c8596d781e18cd66b884d44
Claire2311 <claire.girard@ieseg.fr>
2024-12-06 18:44:01 +0100

    Task 2

--- unittests_in_js/2-calcul_chai.test.js
+++ unittests_in_js/2-calcul_chai.test.js
@@ -1,5 +1,5 @@
 const expect = require("chai").expect;
-const calculateNumber = require("./2-calcul");
+const calculateNumber = require("./2-calcul_chai");
 
 describe("SUM", () => {
   it("should return 6", () => {
