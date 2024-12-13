2de25570326e1bbf7e0de05a8f33239d0234f431
Claire2311 <claire.girard@ieseg.fr>
2024-12-13 13:10:21 +0100

    Task 5

--- unittests_in_js/5-payment.test.js
+++ unittests_in_js/5-payment.test.js
@@ -4,25 +4,26 @@ const Utils = require("./utils");
 const sendPaymentRequestToApi = require("./5-payment");
 
 describe("testsendPaymentRequestToApi", () => {
-  it("should return 120", () => {
-    const consoleSpy = sinon.spy(console, "log");
+  let consoleSpy = null;
+  beforeEach(() => {
+    consoleSpy = sinon.spy(console, "log");
+  });
 
+  afterEach(() => {
+    consoleSpy.restore();
+  });
+
+  it("should return 120", () => {
     sendPaymentRequestToApi(100, 20);
 
     expect(consoleSpy.calledOnce).to.be.true;
     expect(consoleSpy.calledWith("The total is:", 120)).to.be.true;
-
-    consoleSpy.restore();
   });
 
   it("should return 20", () => {
-    const consoleSpy = sinon.spy(console, "log");
-
     sendPaymentRequestToApi(10, 10);
 
     expect(consoleSpy.calledOnce).to.be.true;
     expect(consoleSpy.calledWith("The total is:", 20)).to.be.true;
-
-    consoleSpy.restore();
   });
 });
