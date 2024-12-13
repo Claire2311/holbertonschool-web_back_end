c80a1d32020e00b56e31d4f104e958ee2ed93947
Claire2311 <claire.girard@ieseg.fr>
2024-12-13 13:14:36 +0100

    Task 5

--- unittests_in_js/5-payment.test.js
+++ unittests_in_js/5-payment.test.js
@@ -17,13 +17,13 @@ describe("testsendPaymentRequestToApi", () => {
     sendPaymentRequestToApi(100, 20);
 
     expect(consoleSpy.calledOnce).to.be.true;
-    expect(consoleSpy.calledWith("The total is:", 120)).to.be.true;
+    expect(consoleSpy.calledWith("The total is: 120")).to.be.true;
   });
 
   it("should return 20", () => {
     sendPaymentRequestToApi(10, 10);
 
     expect(consoleSpy.calledOnce).to.be.true;
-    expect(consoleSpy.calledWith("The total is:", 20)).to.be.true;
+    expect(consoleSpy.calledWith("The total is: 20")).to.be.true;
   });
 });
