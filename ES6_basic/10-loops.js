1560085d5c138e1a3f712cc9be17a91ce9c63697
Claire2311 <claire.girard@ieseg.fr>
2025-01-24 14:05:25 +0100

    Task 10

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,6 @@
+export default function appendToEachArrayValue(array, appendString) {
+        for(const [i, el] of array.entries()){
+            array[i] = appendString + el
+        }
+    return array;
+  }
